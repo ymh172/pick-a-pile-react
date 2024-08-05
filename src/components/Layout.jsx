474de252bd9a20f816920ed5be90c7cbafb06105
YMH <notailuu9@gmail.com>
2024-08-05 21:24:04 +0630

    raw

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Menu from "../pages/Menu";
+
 
 const Layout = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
 import "./index.css";
-import Answer from "./pages/Answer.jsx";
-import Menu from "./pages/Menu.jsx";
+
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.jsx";
 
